a2063e7cf114e9722bc6548548334b0c86697296
Joao Luis Moraes <joaoluismoraes@Joaos-MacBook-Air.local>
2019-09-17 21:18:00 -0300

    structs

--- structs/struct.c
+++ structs/struct.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// create a student
+struct Student{
+  char name[50];
+  char major[50];
+  int age;
+  double gpa;
+};
 
 int main() {
+  
+  // assign values to created student
+  struct Student student;
+  student.age = 23;
+  student.gpa = 4.0;
+  strcpy(student.name, "Jon");
+  strcpy(student.major, "Computer Science");
+
+  printf("%s\n", student.name);
+  printf("%s", student.major);
 
   return 0;
 }
